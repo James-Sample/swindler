60c011db6b57819e0a2db1e9892df1b46669d0e5
James Sample <jamesgsample@gmail.com>
2023-06-21 11:15:30 +0100

    refactored tests

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -13,81 +13,39 @@ beforeEach(() => {
   return User.destroy({ truncate: true });
 });
 
-describe('User registration', () => {
-  it('returns 200 OK when signup request is valid', (done) => {
-    request(app)
-      .post('/api/1.0/users')
-      .send({
-        username: 'user1',
-        email: 'user1@gmail.com',
-        password: 'P4ssword',
-      })
-      .then((response) => {
-        expect(response.status).toBe(200);
-        done();
-      });
+const postValidUser = () => {
+  return request(app).post('/api/1.0/users').send({
+    username: 'user1',
+    email: 'user1@gmail.com',
+    password: 'P4ssword',
   });
+};
 
-  it('returns success message when signup request is valid', (done) => {
-    request(app)
-      .post('/api/1.0/users')
-      .send({
-        username: 'user1',
-        email: 'user1@gmail.com',
-        password: 'P4ssword',
-      })
-      .then((response) => {
-        expect(response.body.message).toBe('User Created');
-        done();
-      });
+describe('User registration', () => {
+  it('returns 200 OK when signup request is valid', async () => {
+    const response = await postValidUser();
+    expect(response.status).toBe(200);
   });
-  it('saves the user to the database', (done) => {
-    request(app)
-      .post('/api/1.0/users')
-      .send({
-        username: 'user1',
-        email: 'user1@gmail.com',
-        password: 'P4ssword',
-      })
-      .then(() => {
-        //query user table
-        User.findAll().then((userList) => {
-          expect(userList.length).toBe(1);
-          done();
-        });
-      });
+  it('returns success message when signup request is valid', async () => {
+    const response = await postValidUser();
+    expect(response.body.message).toBe('User Created');
   });
-  it('saves the username and email to the database', (done) => {
-    request(app)
-      .post('/api/1.0/users')
-      .send({
-        username: 'user1',
-        email: 'user1@gmail.com',
-        password: 'P4ssword',
-      })
-      .then(() => {
-        User.findAll().then((userList) => {
-          const savedUser = userList[0];
-          expect(savedUser.username).toBe('user1');
-          expect(savedUser.email).toBe('user1@gmail.com');
-          done();
-        });
-      });
+  it('saves the user to the database', async () => {
+    await postValidUser();
+    const userList = await User.findAll();
+    expect(userList.length).toBe(1);
   });
-  it('hashes the password in the database', (done) => {
-    request(app)
-      .post('/api/1.0/users')
-      .send({
-        username: 'user1',
-        email: 'user1@gmail.com',
-        password: 'P4ssword',
-      })
-      .then(() => {
-        User.findAll().then((userList) => {
-          const savedUser = userList[0];
-          expect(savedUser.password).not.toBe('P4ssword');
-          done();
-        });
-      });
+  it('saves the username and email to the database', async () => {
+    const response = await postValidUser();
+    const userList = await User.findAll();
+    const savedUser = userList[0];
+    expect(savedUser.username).toBe('user1');
+    expect(savedUser.email).toBe('user1@gmail.com');
+  });
+  it('hashes the password in the database', async () => {
+    const response = await postValidUser();
+    const userList = await User.findAll();
+    const savedUser = userList[0];
+    expect(savedUser.password).not.toBe('P4ssword');
   });
 });
--- src/app.js
+++ src/app.js
@@ -1,22 +1,9 @@
 const express = require('express');
-const User = require('./user/User');
-const bcrypt = require('bcrypt');
 const app = express();
+const UserRouter = require('./user/UserRouter');
 
 app.use(express.json());
 
-app.post('/api/1.0/users', (req, res) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = { ...req.body, password: hash };
-    // const user = {
-    //   username: req.body.username,
-    //   email: req.body.email,
-    //   password: hash,
-    // };
-    User.create(user).then(() => {
-      return res.send({ message: 'User Created' });
-    });
-  });
-});
+app.use(UserRouter);
 
 module.exports = app;
